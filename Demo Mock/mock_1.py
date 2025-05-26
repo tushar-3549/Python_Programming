fd97c36725b75fbdbea6a24854e763c66d770391
Md. Tushar Ahmed <99356217+tushar-3549@users.noreply.github.com>
2025-05-26 15:05:43 +0600

    Update mock_1.py

--- Demo Mock/mock_1.py
+++ Demo Mock/mock_1.py
@@ -221,7 +221,7 @@ print(is_prime(num))
 '''
 
 # Sum Of Digits
-
+'''
 def sumOfDigit(n):
     ans = 0
     while n != 0:
@@ -230,3 +230,39 @@ def sumOfDigit(n):
     return ans 
 n = 12
 print(sumOfDigit(n))
+'''
+
+# String to int 
+
+def myAtoi(s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    s = s.strip()
+    if not s:
+        return 0
+    sign = 1
+    if s[0] == '+':
+        sign = 1
+        s = s[1:]
+    elif s[0] == '-':
+        sign = -1
+        s = s[1:]
+    num = 0
+    for c in s:
+        if c.isdigit():
+            num = num*10+int(c)
+        else:
+            break
+    num *= sign 
+
+    INT_MIN, INT_MAX = -2**31, 2**31 - 1
+    if num < INT_MIN:
+        return INT_MIN
+    if num > INT_MAX:
+        return INT_MAX
+
+    return num
+print(myAtoi("32"))
+        
