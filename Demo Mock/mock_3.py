f8e4ff14548e89965b98ce74b72e18ccc63af1b8
Md. Tushar Ahmed <99356217+tushar-3549@users.noreply.github.com>
2025-07-20 12:13:27 +0600

    Update mock_3.py

--- Demo Mock/mock_3.py
+++ Demo Mock/mock_3.py
@@ -78,3 +78,33 @@ class Solution(object):
             num = res
         return num
 '''
+#  make a stack with a queue
+
+from collections import deque
+class Stack:
+    def __init__(self):
+        self.q = deque()
+    
+    def push(self,x):
+        self.q.append(x)
+        for _ in range(len(self.q)-1):
+            return self.q.append(self.q.popleft())
+    
+    def pop(self):
+        return self.q.popleft()
+    def top(self):
+        return self.q[0]
+    def empty(self):
+        return not self.q
+
+s = Stack()
+s.push(1)
+s.push(2)
+s.push(3)
+
+
+
+print(s.pop())   # 3
+print(s.pop())   # 2
+print(s.pop())   # 1
+print(s.empty()) # True
